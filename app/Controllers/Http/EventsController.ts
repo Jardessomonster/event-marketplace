d715c409c520ce56efc18212d85995a12f531369
jardessomonster <jrjardeson@gmail.com>
2022-04-12 15:34:04 -0300

    feat: add my events route :sparkles:

--- app/Controllers/Http/EventsController.ts
+++ app/Controllers/Http/EventsController.ts
@@ -5,12 +5,12 @@ import EventRepository from 'App/Repositories/EventRepository'
 import EventService from 'App/Services/EventService'
 import CreateEventValidator from 'App/Validators/CreateEventValidator'
 import UpdateEventValidator from 'App/Validators/UpdateEventValidator'
+import { userType } from 'Contracts/enums'
 
 export default class EventsController {
   async listEvents({ auth, response, pagination }: HttpContextContract) {
     Logger.debug(`EventsController.index: Called`)
-    if (!auth.user)
-      return response.unauthorized({ message: `User not authorize` })
+    if (!auth.user) return response.unauthorized({ message: `User not authorize` })
 
     const user = auth.user
     return response.ok(
@@ -18,7 +18,7 @@ export default class EventsController {
         .preload('owner')
         .preload('speakers')
         .withCount('tickets')
-        .whereDoesntHave('tickets', subQuery => {
+        .whereDoesntHave('tickets', (subQuery) => {
           subQuery.where('buyer_id', user.id)
         })
         .andWhere('qtd_avalible_tickets', '>', 0)
@@ -38,10 +38,28 @@ export default class EventsController {
     return response.ok(event)
   }
 
+  async myEvents({ response, auth, pagination }: HttpContextContract) {
+    Logger.debug(`EventsController.myEvents: Called`)
+    if (!auth.user) return response.unauthorized({ message: `User not authorize` })
+
+    const user = auth.user
+    const events =
+      user.type === userType.CONSUMER
+        ? await Event.query()
+            .preload('owner')
+            .preload('speakers')
+            .whereHas('tickets', (query) => {
+              query.where('buyer_id', user.id)
+            }).paginate(pagination.page, pagination.perPage)
+        : await Event.query().preload('owner').preload('speakers').where('owner_id', user.id).paginate(pagination.page, pagination.perPage)
+      
+    Logger.info(`EventsController.myEvents: Found events for user ${user.id} of type ${user.type}`)
+    return response.ok(events)
+  }
+
   async store({ request, response, auth }: HttpContextContract) {
     Logger.debug(`EventsController.store: Called`)
-    if (!auth.user)
-      return response.unauthorized({ message: `User not authorize` })
+    if (!auth.user) return response.unauthorized({ message: `User not authorize` })
 
     const data = await request.validate(CreateEventValidator)
     const event = await EventRepository.createEvent(auth.user, data)
@@ -52,17 +70,16 @@ export default class EventsController {
 
   async buyTicket({ response, params, auth }: HttpContextContract) {
     Logger.debug(`EventsController.buyTicket: Called`)
-    if (!auth.user)
-      return response.unauthorized({ message: `User not authorize` })
+    if (!auth.user) return response.unauthorized({ message: `User not authorize` })
 
     const user = auth.user
     const event = await Event.query()
       .where('id', params.id)
-      .whereDoesntHave('tickets', subQuery => {
+      .whereDoesntHave('tickets', (subQuery) => {
         subQuery.where('buyer_id', user.id)
       })
       .firstOrFail()
-  
+
     Logger.info(`EventsController.buyTicket: found event ${event.id}`)
     if (event.qtd_avalible_tickets <= 0) {
       Logger.warn(`EventsController.buyTicket: The tickets for the event ${event.id} is over`)
@@ -76,8 +93,7 @@ export default class EventsController {
   async update({ request, response, params, auth }: HttpContextContract) {
     Logger.debug(`EvemtsController.update: Called`)
     const data = await request.validate(UpdateEventValidator)
-    if (!auth.user)
-      return response.unauthorized({ message: `User not authorize` })
+    if (!auth.user) return response.unauthorized({ message: `User not authorize` })
 
     const event = await auth.user.related('events').query().where('id', params.id).firstOrFail()
     Logger.info(`EvemtsController.update: found event ${event.id}`)
@@ -87,8 +103,7 @@ export default class EventsController {
 
   async destroy({ response, params, auth }) {
     Logger.debug(`EvemtsController.destroy: Called`)
-    if (!auth.user)
-      return response.unauthorized({ message: `User not authorize` })
+    if (!auth.user) return response.unauthorized({ message: `User not authorize` })
 
     const event = await auth.user.related('events').query().where('id', params.id).firstOrFail()
     Logger.info(`EvemtsController.destroy: found event ${event.id}`)
