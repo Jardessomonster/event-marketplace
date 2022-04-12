e7b1f6a466edae81380f6b2bea4593d53682ce18
jardessomonster <jrjardeson@gmail.com>
2022-04-12 02:45:20 -0300

    feat: add wallet validation response :sparkles:

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -26,6 +26,9 @@ export default class ExceptionHandler extends HttpExceptionHandler {
     if (error.code === 'ERR_HTTP_INVALID_STATUS_CODE')
       return response.status(500).send({ message: 'An error has occurred :/' })
 
+    if (error.code === 'NOT_ENOUGH_MONEY')
+      return response.forbidden(error)
+
     return response.status(error.status).send(error)
   }
 }
