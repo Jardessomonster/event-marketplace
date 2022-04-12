3ec9aeb00fb5a0716203498af3cc88514acbb4a3
jardessomonster <jrjardeson@gmail.com>
2022-04-12 12:14:22 -0300

    fix: validate other user types :bug:

--- app/Middleware/UserPermission.ts
+++ app/Middleware/UserPermission.ts
@@ -6,7 +6,10 @@ export default class UserPermission {
     if (!ctx.auth.user)
       return ctx.response.unauthorized({ message: 'You are not authorized' })
 
-    if (ctx.auth.user.type === userType.CONSUMER && guards[0] === userType.COMPANY)
+    if (ctx.auth.user.type === userType.CONSUMER && (guards[0] === userType.COMPANY || guards[0] === userType.UNIVERSITY))
+      return ctx.response.forbidden({ message: 'Your user type cannot peform this action' })
+    
+    if ((ctx.auth.user.type === userType.COMPANY ||  ctx.auth.user.type === userType.UNIVERSITY ) && guards[0] === userType.CONSUMER)
       return ctx.response.forbidden({ message: 'Your user type cannot peform this action' })
     
     return await next()
